4953871213f9b0373cd343147bd865a78958a347
SAAD MOHAMMAD <onetap170@gmail.com>
2026-09-12 23:06:21 +0530

    Day 10 - Linked List Cycle

---
+++ Day-10/LinkedListCycle.java
@@ -0,0 +1,30 @@
+public class LinkedListCycle {
+    static class ListNode {
+        int value;
+        ListNode next;
+        ListNode(int value) { this.value = value; }
+    }
+
+    static boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode a = new ListNode(3);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(0);
+        ListNode d = new ListNode(-4);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = b;
+        System.out.println(hasCycle(a));
+    }
+}
